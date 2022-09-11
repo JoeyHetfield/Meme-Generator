9dfa4617582f03748b1af5aaf04d6afbd876b142
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-11 15:11:52 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputText = document.getElementById('text-input');
 const receberText = document.getElementById('meme-text');
 const inputImage = document.getElementById('meme-insert');
 const receberImage = document.getElementById('meme-image');
+const container = document.querySelector('#meme-image-container')
 
 // Colocar o texto do input na tela
 inputText.addEventListener('input', function () {
@@ -14,3 +15,15 @@ function insertImage(event) {
 }
 
 inputImage.addEventListener('change', insertImage);
+
+function changeFire() {
+    container.style.border = '3px dashed red' 
+}
+
+function changeWater() {
+    container.style.border = '5px double blue'
+}
+
+function changeEarth() {
+    container.style.border = '6px groove green'
+}
