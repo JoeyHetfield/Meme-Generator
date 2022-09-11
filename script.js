e85e8bb965722cd8baf7c84c53eb861de2670c73
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-11 14:48:14 -0300

    tentando corrigir lint

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-let inputText = document.getElementById('text-input');
-let receberText = document.getElementById('meme-text');
-let inputImage = document.getElementById('meme-insert');
-let receberImage = document.getElementById('meme-image')
+const inputText = document.getElementById('text-input');
+const receberText = document.getElementById('meme-text');
+const inputImage = document.getElementById('meme-insert');
+const receberImage = document.getElementById('meme-image');
 
+// Colocar o texto do input na tela
+inputText.addEventListener('input', function () {
+  receberText.innerText = inputText.value;
+});
 
-//Colocar o texto do input na tela
-inputText.addEventListener('input', function(){
-    receberText.innerText = inputText.value
-})
-
-
-//Me baseiei no video https://www.youtube.com/watch?v=io5FcMAdLyQ
-function insertImage (event){
-    receberImage.src = URL.createObjectURL(event.target.files[0]);
+// Me baseiei no video https://www.youtube.com/watch?v=io5FcMAdLyQ
+function insertImage(event) {
+  receberImage.src = URL.createObjectURL(event.target.files[0]);
 }
 
-inputImage.addEventListener('change', insertImage)
-
+inputImage.addEventListener('change', insertImage);
