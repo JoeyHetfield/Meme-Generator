ccfd4732434d22ec19ab8f4240e2a48e51eb378d
Joey Hetfield <joeydefenix@hotmail.com>
2022-09-11 13:50:49 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 let inputText = document.getElementById('text-input');
 let receberText = document.getElementById('meme-text');
+let inputImage = document.getElementById('meme-insert');
+let receberImage = document.getElementById('meme-image')
 
+
+//Colocar o texto do input na tela
 inputText.addEventListener('input', function(){
     receberText.innerHTML = inputText.value
-
 })
+
+
+//Me baseiei no video https://www.youtube.com/watch?v=io5FcMAdLyQ
+function insertImage (event){
+    receberImage.src = URL.createObjectURL(event.target.files[0]);
+}
+
+inputImage.addEventListener('change', insertImage)
+
